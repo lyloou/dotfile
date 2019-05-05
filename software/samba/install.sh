4fe78fb9285f0f85f59bc68ff44119b3c9f6645a
lyloou <lyloou@qq.com>
2019-05-05 14:51:57 +0800

    update: tmux and samba

--- software/samba/install.sh
+++ software/samba/install.sh
@@ -2,9 +2,11 @@
 # Firstly, you need modify smb.conf file
 
 sudo apt-get install samba -y
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
 sudo cat smb.conf >> /etc/samba/smb.conf
 sudo sed -i "s|\$USER|$USER|g" /etc/samba/smb.conf
 mkdir -p $HOME/w/smbshare
+chmod 777 $HOME/w/smbshare
 
 # sudo smbpasswd -a $USER
 # sudo service smbd restart
