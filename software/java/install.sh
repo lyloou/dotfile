329f1110dce6632c6c9fbbdaa268ba47dd34c652
lyloou <lyloou@qq.com>
2020-06-06 12:02:14 +0800

    update: java

--- software/java/install.sh
+++ software/java/install.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 # https://tecadmin.net/install-openjdk-java-ubuntu/
 # apt-cache search openjdk
 
 # remove installed java
 # sudo apt-get purge openjdk-\* icedtea-\* icedtea6-\*
 sudo apt-get install openjdk-11-jre openjdk-11-jdk -y
+
 mkdir -p $HOME/c/java/
-ln-sf /usr/lib/jvm/java-11-openjdk-amd64 $HOME/c/java/jdk
+
+ln -sf /usr/lib/jvm/java-11-openjdk-amd64 $HOME/c/java/jdk
