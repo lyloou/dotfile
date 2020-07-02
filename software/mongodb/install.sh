db32f6bbc932ce94492215077ca77d079ff8943d
lyloou <lyloou@qq.com>
2020-07-02 16:18:19 +0800

    add mongodb

---
+++ software/mongodb/install.sh
@@ -0,0 +1,5 @@
+cd ~/t/
+wget https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-3.6.4.tgz
+tar -zxvf mongodb-linux-x86_64-amazon-3.6.4
+mv mongodb-linux-x86_64-amazon-3.6.4 ~/c/mongodb
+cd ~/c/mongodb
